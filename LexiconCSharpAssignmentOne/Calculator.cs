e0721e5f31d9a07b2a0ccfe7c34f2ad0b7f77d98
Shikyo <dennan_lol@hotmail.com>
2021-06-22 17:55:47 +0200

    Calc class created and all calc methods moved there. Addition and substraction method with array added to calc class

---
+++ LexiconCSharpAssignmentOne/Calculator.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LexiconCSharpAssignmentOne
+{
+    public class Calculator
+    {
+        public string AdditionMethod(double num1, double num2)
+        {
+            //Add them together directly in the writeline because there is no need to save the results
+            string output = $"{num1} + {num2} = " + (num1 + num2);
+
+            return output;
+        }
+        
+        public string AdditionMethod(double[] numbers)
+        {
+            double totalValue = 0;
+            string output = "";
+
+            if (numbers.Length > 0)
+            {
+                output = numbers[0].ToString();
+                totalValue = numbers[0];
+
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    output += $" + {numbers[i]}";
+                    totalValue += numbers[i];
+                }
+                output += $" = {totalValue}";
+            }
+            else
+                throw new IndexOutOfRangeException("No numbers were inputed to addition on a list");
+
+            return output;
+        }
+
+        public string SubstractionMethod(double num1, double num2)
+        {
+            //Substract them directly in a string because there is no need to save the results
+            string output = $"{num1} - {num2} = " + (num1 - num2);
+
+            return output;
+        }
+        
+        public string SubstractionMethod(double[] numbers)
+        {
+            double totalValue = 0;
+            string output = "";
+
+            if (numbers.Length > 0)
+            {
+                output = numbers[0].ToString();
+                totalValue = numbers[0];
+
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    output += $" - {numbers[i]}";
+                    totalValue -= numbers[i];
+                }
+                output += $" = {totalValue}";
+            }
+            else
+                throw new IndexOutOfRangeException("No numbers were inputed to substraction on a list");
+
+            return output;
+        }
+
+        //Waits for the user to input two numbers then divides them together
+        public string DivisionMethod(double num1, double num2)
+        {
+            string output;
+            if (!num2.Equals(0))
+                output = $"{num1} / {num2} = " + (num1 / num2);
+            else
+                throw new DivideByZeroException();
+
+            return output;
+        }
+
+        //Waits for the user to input two numbers then multiplicate them together
+        public string MultiplicationMethod(double num1, double num2)
+        {
+            //Multiplicates them together directly in the writeline because there is no need to save the results
+            string output = $"{num1} * {num2} = " + (num1 * num2);
+
+            return output;
+        }
+
+        //Takes one input from the user then square roots it
+        public string SquareRootsMethod(double number)
+        {
+            string outPutAsString = "";
+
+            //Square root it directly to a string because there is no need to save the results
+            outPutAsString = $"square root of {number} = " + Math.Sqrt(number);
+            
+            return outPutAsString;
+        }
+
+        /*
+        //Takes two numbers from the user, the number that will be powered and the power.
+        //Then Math.pow them.
+        public void PowerOfMethod()
+        {
+            double numberToBePowered = 0;
+            double power = 0;
+
+            //Let's the user input two numbers
+            Console.Write("Input the number that is going to be powered: ");
+            string stringInputOne = Console.ReadLine();
+            Console.Write("Input power: ");
+            string stringInputPower = Console.ReadLine();
+
+            if (double.TryParse(stringInputOne, out numberToBePowered) && double.TryParse(stringInputPower, out power))
+            {
+                //Math.pow directly in the writeline because there is no need to save the results
+                Console.WriteLine("{0}^{1} = " + Math.Pow(numberToBePowered, power), numberToBePowered, power);
+            }
+            else
+            {
+                Console.WriteLine("Either {0} or {1} isn't a number!", stringInputOne, stringInputPower);
+            }
+        }
+        */
+
+        //Calculate power of but with a recursive function.
+        //It recives the current result(starts as numberToBePowered),
+        //the number that is going to be powered and power as a count so the function knows when to stop
+        public double PowerOfMethod(double currentValue, double numberToBePowered, int count)
+        {
+            //Multiplicate the current value with numberToBePowered each time the function is called just like with 2^3, 2*2=4*2=8
+            currentValue *= numberToBePowered;
+
+            //Reduce count each time so the function knows when to stop
+            count--;
+
+            //If count has reached 0, return, if not then the function continues to call itself
+            if (count > 0)
+            {
+                return PowerOfMethod(currentValue, numberToBePowered, count);
+            }
+            else
+                return currentValue;
+        }
+    }
+}
--- LexiconCSharpAssignmentOne/Program.cs
+++ LexiconCSharpAssignmentOne/Program.cs
@@ -2,14 +2,50 @@
 
 namespace LexiconCSharpAssignmentOne
 {
+    //////////////////////////////////////////
+    //Create a calculator class that just calculate
+    //See if generic <T> can/should be added
+    //Make a ask/inputFromUser method that is generic enought to be 
+    //called everytime we need something from the user, even if
+    //what we ask for is completly different. 
+    //Use Console.WriteLine("Input {StringWhatWeAskFromUser} ");
+    //This way we only need one method for every user input situation.
+    //We can save this method and reuse it in other projects!
+    //The only problem is where to parse, can't be in main if we want to unit test it
+    //
+    //What happens if we make Program public? Can we access it from unit tester? If so that will solve the parse problem
+    //////////////////////////////////////////
+    /*
+     * This is how we can show info inside a class without either the class or main being reliant on each other, thus getting loose coupling in both
+    public string Info()
+    {
+        return $"---- Car Info ----\nId: {id}\nBrand: {brand}\nModel: {model}\nColor {color}\nYear: {year}\nHp: {hp}";
+    }
+    or
+    public override string ToString()
+    {
+        return Info();
+    }
+    */
+
+    //////////////////////////////////
+    //In git changes -> ... -> new branch !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    //////////////////////////////////
+
     class Program
     {
         static void Main(string[] args)
         {
+            Calculator calc = new Calculator();
             bool isAlive = true;
+            double num1 = 0;
+            double num2 = 0;
 
             while (isAlive)
             {
+                num1 = 0;
+                num2 = 0;
+
                 Console.WriteLine("Welcome to my Calculator!\n Please select one of the following functionalities: " +
                     "\n 1: Addition " +
                     "\n 2: Subtraction" +
@@ -17,43 +53,80 @@ namespace LexiconCSharpAssignmentOne
                     "\n 4: Multiplication" +
                     "\n 5: Square Roots" +
                     "\n 6: Power of" +
+                    "\n 7: Addition on a list of numbers" +
+                    "\n 8: Subtraction on a list of numbers" +
                     "\n 99: Exit");
 
                 int.TryParse(Console.ReadLine(), out int userSelection);
 
-                switch (userSelection)
+                try
                 {
-                    case 1:
-                        Console.Clear();
-                        AdditionMethod();
-                        break;
-                    case 2:
-                        Console.Clear();
-                        SubstractionMethod();
-                        break;
-                    case 3:
-                        Console.Clear();
-                        DivisionMethod();
-                        break;
-                    case 4:
-                        Console.Clear();
-                        MultiplicationMethod();
-                        break;
-                    case 5:
-                        Console.Clear();
-                        SquareRootsMethod();
-                        break;
-                    case 6:
-                        Console.Clear();
-                        Console.WriteLine("Result: " + UserInputForPowerOf());
-                        break;
-                    case 99:
-                        Console.WriteLine("Thank you for trying out my calculator!");
-                        isAlive = false;
-                        break;
-                    default:
-                        Console.WriteLine("Not a valid selection, please try again");
-                        break;
+                    switch (userSelection)
+                    {
+                        case 1:
+                            Console.Clear();
+                            num1 = AskForInputDouble("number");
+                            num2 = AskForInputDouble("number");
+                            Console.WriteLine(calc.AdditionMethod(num1, num2));
+                            break;
+                        case 2:
+                            Console.Clear();
+                            num1 = AskForInputDouble("number");
+                            num2 = AskForInputDouble("number");
+                            Console.WriteLine(calc.SubstractionMethod(num1, num2));
+                            break;
+                        case 3:
+                            Console.Clear();
+                            num1 = AskForInputDouble("numerator");
+                            num2 = AskForInputDouble("denominator");
+                            Console.WriteLine(calc.DivisionMethod(num1, num2));
+                            break;
+                        case 4:
+                            Console.Clear();
+                            num1 = AskForInputDouble("number");
+                            num2 = AskForInputDouble("number");
+                            Console.WriteLine(calc.MultiplicationMethod(num1, num2));
+                            break;
+                        case 5:
+                            Console.Clear();
+                            Console.WriteLine(calc.SquareRootsMethod(AskForInputDouble("number to be squared")));
+                            break;
+                        case 6:
+                            Console.Clear();
+                            num1 = AskForInputDouble("number");
+                            int power = AskForInputInt("a power");
+                            Console.WriteLine("Result: " + UserInputForPowerOf(calc, num1, power));
+                            break;
+                        case 7:
+                            Console.Clear();
+                            double[] addNumArr = AskForAnArray("added together");
+                            Console.WriteLine(calc.AdditionMethod(addNumArr));
+                            break;
+                        case 8:
+                            Console.Clear();
+                            double[] subNumArr = AskForAnArray("substracted together");
+                            Console.WriteLine(calc.SubstractionMethod(subNumArr));
+                            break;
+                        case 99:
+                            Console.WriteLine("Thank you for trying out my calculator!");
+                            isAlive = false;
+                            break;
+                        default:
+                            Console.WriteLine("Not a valid selection, please try again");
+                            break;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch(DivideByZeroException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
                 }
 
                 Console.ReadKey();
@@ -61,204 +134,56 @@ namespace LexiconCSharpAssignmentOne
             }
         }
 
-        //Waits for the user to input two numbers then adds them together
-        private static void AdditionMethod()
+        //Take the input from the user and does the checks on those inputs before starting the recursive function to calculate power of
+        public static double UserInputForPowerOf(Calculator calc, double num1, int numPower)
         {
-            decimal firstNumber = 0;
-            decimal secondNumber = 0;
+            if (numPower > 0)
+                return calc.PowerOfMethod(num1, num1, --numPower);
+            else if (numPower < 0)
+                return 1 / calc.PowerOfMethod(num1, num1, ++numPower * -1);
 
-            //Let's the user input two numbers
-            Console.Write("Input the first number: ");
-            string stringInputOne = Console.ReadLine();
-            Console.Write("Input the second number: ");
-            string stringInputTwo = Console.ReadLine();
-
-            //int.TryParse returns true or false depending on if it could parse the string to an int
-            //So we use that to see if both inputs are infact numbers. 
-            if (decimal.TryParse(stringInputOne, out firstNumber) && decimal.TryParse(stringInputTwo, out secondNumber))
-            {
-                //Add them together directly in the writeline because there is no need to save the results
-                Console.WriteLine("{0} + {1} = " + (firstNumber + secondNumber), firstNumber, secondNumber);
-            }
-            else
-            {
-                Console.WriteLine("Either {0} or {1} isn't a number!", stringInputOne, stringInputTwo);
-            }
+            return 1;
         }
 
-        //Waits for the user to input two numbers then substracts them together
-        private static void SubstractionMethod()
+        private static double AskForInputDouble(string whatToAsk)
         {
-            decimal firstNumber = 0;
-            decimal secondNumber = 0;
-
-            //Let's the user input two numbers
-            Console.Write("Input the first number: ");
-            string stringInputOne = Console.ReadLine();
-            Console.Write("Input the second number: ");
-            string stringInputTwo = Console.ReadLine();
-
-            if (decimal.TryParse(stringInputOne, out firstNumber) && decimal.TryParse(stringInputTwo, out secondNumber))
-            {
-                //Substract them directly in the writeline because there is no need to save the results
-                Console.WriteLine("{0} - {1} = " + (firstNumber - secondNumber), firstNumber, secondNumber);
-            }
-            else
-            {
-                Console.WriteLine("Either {0} or {1} isn't a number!", stringInputOne, stringInputTwo);
-            }
-        }
-
-        //Waits for the user to input two numbers then divides them together
-        private static void DivisionMethod()
-        {
-            decimal numeratorNumber = 0;
-            decimal denominatorNumber = 0;
-
-            //Let's the user input two numbers
-            Console.Write("Input the numerator: ");
-            string stringNumerator = Console.ReadLine();
-            Console.Write("Input the denominator: ");
-            string stringDenominator = Console.ReadLine();
-
-            if (decimal.TryParse(stringNumerator, out numeratorNumber) && decimal.TryParse(stringDenominator, out denominatorNumber))
-            {
-                //Quick check if denominator is 0
-                if (denominatorNumber != 0)
-                {
-                    Console.WriteLine("{0} / {1} = " + (numeratorNumber / denominatorNumber), numeratorNumber, denominatorNumber);
-                }
-                else
-                {
-                    Console.WriteLine("Error: You tried to divied with 0!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Either {0} or {1} isn't a number!", stringNumerator, stringDenominator);
-            }
-        }
-
-        //Waits for the user to input two numbers then multiplicate them together
-        private static void MultiplicationMethod()
-        {
-            decimal firstNumber = 0;
-            decimal secondNumber = 0;
-
-            //Let's the user input two numbers
-            Console.Write("Input the first number: ");
-            string stringInputOne = Console.ReadLine();
-            Console.Write("Input the second number: ");
-            string stringInputTwo = Console.ReadLine();
-
-            if (decimal.TryParse(stringInputOne, out firstNumber) && decimal.TryParse(stringInputTwo, out secondNumber))
-            {
-                //Multiplicates them together directly in the writeline because there is no need to save the results
-                Console.WriteLine("{0} * {1} = " + (firstNumber * secondNumber), firstNumber, secondNumber);
-            }
-            else
-            {
-                Console.WriteLine("Either {0} or {1} isn't a number!", stringInputOne, stringInputTwo);
-            }
-        }
-
-        //Takes one input from the user then square roots it
-        private static void SquareRootsMethod()
-        {
+            string outPutAsString = "";
             double number = 0;
 
-            //Let's the user input a number
-            Console.Write("Input a number: ");
-            string stringInput = Console.ReadLine();
+            Console.Write($"Input a {whatToAsk} (decimals): ");
+            outPutAsString = Console.ReadLine();
 
-            if (double.TryParse(stringInput, out number))
-            {
-                //Square root it directly in the writeline because there is no need to save the results
-                Console.WriteLine("square root of {0} = " + Math.Sqrt(number), number);
-            }
-            else
-            {
-                Console.WriteLine("Either {0} isn't a number!", stringInput);
-            }
+            number = double.Parse(outPutAsString);
+
+            return number;
         }
 
-        /*
-        //Takes two numbers from the user, the number that will be powered and the power.
-        //Then Math.pow them.
-        private static void PowerOfMethod()
+        private static int AskForInputInt(string whatToAsk)
         {
-            double numberToBePowered = 0;
-            double power = 0;
+            string outPutAsString = "";
+            int number = 0;
 
-            //Let's the user input two numbers
-            Console.Write("Input the number that is going to be powered: ");
-            string stringInputOne = Console.ReadLine();
-            Console.Write("Input power: ");
-            string stringInputPower = Console.ReadLine();
+            Console.Write($"Input a {whatToAsk} (no decimals): ");
+            outPutAsString = Console.ReadLine();
 
-            if (double.TryParse(stringInputOne, out numberToBePowered) && double.TryParse(stringInputPower, out power))
-            {
-                //Math.pow directly in the writeline because there is no need to save the results
-                Console.WriteLine("{0}^{1} = " + Math.Pow(numberToBePowered, power), numberToBePowered, power);
-            }
-            else
-            {
-                Console.WriteLine("Either {0} or {1} isn't a number!", stringInputOne, stringInputPower);
-            }
-        }
-        */
+            number = int.Parse(outPutAsString);
 
-        //Take the input from the user and does the checks on those inputs before starting the recursive function to calculate power of
-        private static double UserInputForPowerOf()
-        {
-            double numberToBePowered = 0;
-            int power = 0;
-
-            //Let's the user input two numbers
-            Console.Write("Input the number that is going to be powered(Positiv number): ");
-            string stringInputOne = Console.ReadLine();
-            Console.Write("Input power (integer, positiv or negativ): ");
-            string stringInputPower = Console.ReadLine();
-
-            if (double.TryParse(stringInputOne, out numberToBePowered) && int.TryParse(stringInputPower, out power))
-            {
-                //If power is above 0 we call powerOfMethod then return the value
-                //If power is negativ then we still call powerOfMethod (But change power to positiv)
-                ////but we divide 1 by the result and send that back.
-                //And if power is 0 we just return 1
-                if (power > 0)
-                    return powerOfMethod(numberToBePowered, numberToBePowered, --power);
-                else if (power < 0)
-                    return 1 / powerOfMethod(numberToBePowered, numberToBePowered, ++power * -1);
-                else
-                    return 1;
-            }
-            else
-            {
-                Console.WriteLine("Either {0} or {1} isn't a number!", stringInputOne, stringInputPower);
-            }
-
-            return 0.0;
+            return number;
         }
 
-        //Calculate power of but with a recursive function.
-        //It recives the current result(starts as numberToBePowered),
-        //the number that is going to be powered and power as a count so the function knows when to stop
-        private static double PowerOfMethod(double currentValue, double numberToBePowered, int count)
+        private static double[] AskForAnArray(string whatToAsk)
         {
-            //Multiplicate the current value with numberToBePowered each time the function is called just like with 2^3, 2*2=4*2=8
-            currentValue *= numberToBePowered;
+            Console.Write($"Input multiple numbers to be {whatToAsk}: ");
+            string str = Console.ReadLine();
+            string[] numArrString = str.Split();
+            double[] numArr = new double[numArrString.Length];
 
-            //Reduce count each time so the function knows when to stop
-            count--;
-
-            //If count has reached 0, return, if not then the function continues to call itself
-            if (count > 0)
+            for (int i = 0; i < numArrString.Length; i++)
             {
-                return powerOfMethod(currentValue, numberToBePowered, count);
+                numArr[i] = double.Parse(numArrString[i]);
             }
-            else
-                return currentValue;
+
+            return numArr;
         }
     }
 }
