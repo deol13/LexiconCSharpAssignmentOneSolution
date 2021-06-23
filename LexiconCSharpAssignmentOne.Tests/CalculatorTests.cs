fefb68b346f6e20748e8eace43df55f86a0c9bcf
Shikyo <dennan_lol@hotmail.com>
2021-06-23 09:18:58 +0200

    Base xUnit test added

---
+++ LexiconCSharpAssignmentOne.Tests/CalculatorTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using LexiconCSharpAssignmentOne;
+
+namespace LexiconCSharpAssignmentOne.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
